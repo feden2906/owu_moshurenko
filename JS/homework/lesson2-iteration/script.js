221a19709bfa217a20e642847036e10c49f7352d
iFatumm <moshurenko@gmail.com>
2021-06-08 20:52:42 +0300

    cw2

--- JS/homework/lesson2-iteration/script.js
+++ JS/homework/lesson2-iteration/script.js
@@ -146,28 +146,55 @@
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
 //     Записати цей об'єкт в новий масив
 // Масиви:
-// let usersWithId = [
-//     {id: 1, name: 'vasya', age: 31, status: false},
-//     {id: 2, name: 'petya', age: 30, status: true},
-//     {id: 3, name: 'kolya', age: 29, status: true},
-//     {id: 4, name: 'olya', age: 28, status: false}
-// ];
-//
-// let citiesWithId = [
-//     {user_id: 3, country: 'USA', city: 'Portland'},
-//     {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
-//     {user_id: 2, country: 'Poland', city: 'Krakow'},
-//     {user_id: 4, country: 'USA', city: 'Miami'}
-// ];
-//
-// let usersWithCities = [];
-//
-// for (let i = 0; i < usersWithId.length; i++) {
-//     for (let j = 0; j < citiesWithId.length; j++) {
-//         if (usersWithId[i].id === citiesWithId[j].user_id) {
-//             usersWithId[i].address = citiesWithId[j];
-//             usersWithCities.push(usersWithId[i]);
-//         }
-//     }
-// }
-// console.log(usersWithCities);
+let usersWithId = [
+    {id: 1, name: 'vasya', age: 31, status: false},
+    {id: 2, name: 'petya', age: 30, status: true},
+    {id: 3, name: 'kolya', age: 29, status: true},
+    {id: 4, name: 'olya', age: 28, status: false}
+];
+
+let citiesWithId = [
+    {user_id: 3, country: 'USA', city: 'Portland'},
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+    {user_id: 4, country: 'USA', city: 'Miami'}
+];
+
+let usersWithCities = []
+for (let i = 0; i < usersWithId.length; i++) {
+    usersWithCities[i] = {...usersWithId[i]}
+}
+
+for (let i = 0; i < usersWithCities.length; i++) {
+    for (let j = 0; j < citiesWithId.length; j++) {
+        if (usersWithCities[i].id === citiesWithId[j].user_id) {
+            usersWithCities[i].address = citiesWithId[j];
+        }
+    }
+}
+
+console.log(usersWithId);
+console.log(citiesWithId)
+console.log(usersWithCities);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
